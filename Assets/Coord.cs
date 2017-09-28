32f0680fbfbd812aaae377697fae128199442a62
Str4tos <eschenkoden@gmail.com>
2017-09-28 23:46:04 +0300

    Push start idea project
    Start development

---
+++ Assets/Coord.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Coord
+{
+    public float min;
+    public float max;
+}
---
+++ Assets/Coordynates.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coordynates : MonoBehaviour
+{
+    public Coord cooords;
+}
+
+
+
+ 
